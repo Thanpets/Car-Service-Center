9c2c1153799e223f912f961ae1cc991116d1b631
DoumSof99 <71881384+DoumSof99@users.noreply.github.com>
2021-06-16 18:50:23 +0300

    Add Customer

--- CarServiceCenter/CarServiceCenter/WUI/MdiMainForm.cs
+++ CarServiceCenter/CarServiceCenter/WUI/MdiMainForm.cs
@@ -42,7 +42,7 @@ namespace CarServiceCenter.WUI {
             switch (result) {
                 case DialogResult.OK:
 
-                    CarService.Customers.Add(customer);
+                    serviceCenter.Customers.Add(customer);
 
                     break;
 
