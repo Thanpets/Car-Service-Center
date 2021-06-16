dfa993e611a90ed08c373e46aade4b2d1e1f80db
Frantsesko Permeti <frantseskopermeti@gmail.com>
2021-06-16 16:11:11 +0300

    feat: Add Transaction class

---
+++ CarServiceCenter/CarServiceCenter/Impl/Transaction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarServiceCenter.Impl {
+    class Transaction {
+
+        public DateTime Date { get; set; }
+
+        public Guid CustomerID { get; set; }
+
+        public Guid CarID { get; set; }
+
+        public decimal TotalPrice { get; set; }
+
+        public List<TransactionLine> TransactionLines { get; set; }
+
+        public Transaction() {
+
+        }
+        public Transaction(DateTime date, Guid customerID, Guid carID, decimal totalPrice, List<TransactionLine> transactionLines ) :base(){
+
+            Date = date;
+            CustomerID = customerID;
+            CarID = carID;
+            TotalPrice = totalPrice;
+            TransactionLines = transactionLines;
+
+
+        }
+    }
+}
