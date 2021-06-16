054c84a3643878a9d1d1904471b27ac669a5eb26
Frantsesko Permeti <frantseskopermeti@gmail.com>
2021-06-16 17:49:25 +0300

    feat: complete implementation of CarServiceCenter class

--- CarServiceCenter/CarServiceCenter/Impl/CarServiceCenter.cs
+++ CarServiceCenter/CarServiceCenter/Impl/CarServiceCenter.cs
@@ -5,11 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CarServiceCenter.Impl {
-    class CarServiceCenter {
+    class CarServiceCenter : Entity {
 
         public string Name { get; set; }
 
-        public List<Customer> Customers  { get; set; }
+        public List<Customer> Customers { get; set; }
 
         public List<Car> Cars { get; set; }
 
@@ -19,9 +19,24 @@ namespace CarServiceCenter.Impl {
 
         public List<ServiceTask> ServiceTasks { get; set; }
 
-      
+        public List<MonthlyLedger> MonthlyLedgers { get; set; }
+
+        public CarServiceCenter() {
+
+        }
+
+        public CarServiceCenter(string name, List<Customer> customers, List<Car> cars, List<Engineer> engineers, List<Transaction> transactions, List<ServiceTask> serviceTasks, List<MonthlyLedger> monthlyLedgers) : base() {
 
 
+            Name = name;
+            Customers = customers;
+            Cars = cars;
+            Engineers = engineers;
+            Transactions = transactions;
+            ServiceTasks = serviceTasks;
+            MonthlyLedgers = monthlyLedgers;
 
+
+        }
     }
 }
