20919b1326697559640e9b1f03d72059233a08eb
Angelos Renginas <angelosreg@gmail.com>
2021-06-16 16:10:38 +0300

    feat: create ServiceTask class, change entity class to public

---
+++ CarServiceCenter/CarServiceCenter/Impl/ServiceTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarServiceCenter.Impl {
+
+    public class ServiceTask : Entity {
+
+        public String Code  { get; set; }
+        public String Description { get; set; }
+        public decimal PricePerHour { get; set; }
+
+
+        public ServiceTask() {
+
+        }
+
+        public ServiceTask(string code, string description, decimal pricePerHour ): base() {
+            Code = code;
+            Description = description;
+            PricePerHour = pricePerHour;
+        }
+
+
+
+    }
+}
