5c13cffc45d02070d9511b2f47f346a8df40ac55
Thanasis Petsinas <petfountas@hotmail.com>
2021-06-16 16:12:32 +0300

    Created Car Class

---
+++ CarServiceCenter/CarServiceCenter/Impl/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarServiceCenter.Impl {
+    class Car {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string CarRegistrationPlate { get; set; }
+
+        public Car() {
+
+        }
+
+        
+
+    }
+}
