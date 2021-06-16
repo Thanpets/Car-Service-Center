475a24d8ba0ac560e44faf669532edd4911ca376
Frantsesko Permeti <frantseskopermeti@gmail.com>
2021-06-16 20:37:35 +0300

    #8 add serialization and deserialization functionality

--- CarServiceCenter/CarServiceCenter/WUI/MdiMainForm.cs
+++ CarServiceCenter/CarServiceCenter/WUI/MdiMainForm.cs
@@ -4,13 +4,18 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 using System.Windows.Forms;
 
 namespace CarServiceCenter.WUI {
     public partial class MdiMainForm : Form {
+
+
+        private const string _JsonFile = "CarServiceCenterData.json";
 
 
         private ServiceCenter serviceCenter = null;
@@ -135,23 +140,80 @@ namespace CarServiceCenter.WUI {
 
         }
 
+        private void SerializeToJson(object objectToBeSerialized) {
+
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+            string data = serializer.Serialize(objectToBeSerialized);
+
+            string path = Path.Combine(Environment.CurrentDirectory, _JsonFile);
+
+            File.WriteAllText(path, data);
+        }
+
+
+        private void DeserializeFromJson() {
+
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+            string path = Path.Combine(Environment.CurrentDirectory, _JsonFile);
+
+            string data = string.Empty;
+
+            if (File.Exists(path)) {
+
+                data = File.ReadAllText(path);
+
+                serviceCenter = serializer.Deserialize<ServiceCenter>(data);
+            }
+            else {
+
+
+                File.Create(path).Dispose();
+
+                serviceCenter = new ServiceCenter() {
+
+
+                    Name = "CarServiceName",
+                    Customers = new List<Customer>(),
+                    Cars = new List<Car>(),
+                    Engineers = new List<Engineer>(),
+                    Transactions = new List<Transaction>(),
+                    ServiceTasks = new List<ServiceTask>(),
+                    MonthlyLedgers = new List<MonthlyLedger>()
+
+
+                };
+
+                SerializeToJson(serviceCenter);
+
+            }
+
+
+
+
+
+        }
+
+
         private void MdiMainForm_Load(object sender, EventArgs e) {
 
+            DeserializeFromJson();
 
-            serviceCenter = new ServiceCenter() {
-
-
-                Name = "CarServiceName",
-            Customers = new List<Customer>(),
-            Cars = new List<Car>(),
-            Engineers = new List<Engineer>(),
-            Transactions = new List<Transaction>(),
-            ServiceTasks = new List<ServiceTask>(),
-            MonthlyLedgers = new List<MonthlyLedger>()
+        //    serviceCenter = new ServiceCenter() {
 
 
+        //        Name = "CarServiceName",
+        //    Customers = new List<Customer>(),
+        //    Cars = new List<Car>(),
+        //    Engineers = new List<Engineer>(),
+        //    Transactions = new List<Transaction>(),
+        //    ServiceTasks = new List<ServiceTask>(),
+        //    MonthlyLedgers = new List<MonthlyLedger>()
 
-        };
+
+
+        //};
 
 
 
