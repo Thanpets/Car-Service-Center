c96760491efda5ef77fd5b60b0533799e091e373
Frantsesko Permeti <frantseskopermeti@gmail.com>
2021-06-16 17:41:08 +0300

    feat: add CarServiseCenter class but incomplete implementation

---
+++ CarServiceCenter/CarServiceCenter/Impl/CarServiceCenter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarServiceCenter.Impl {
+    class CarServiceCenter {
+
+        public string Name { get; set; }
+
+        public List<Customer> Customers  { get; set; }
+
+        public List<Car> Cars { get; set; }
+
+        public List<Engineer> Engineers { get; set; }
+
+        public List<Transaction> Transactions { get; set; }
+
+        public List<ServiceTask> ServiceTasks { get; set; }
+
+      
+
+
+
+    }
+}
